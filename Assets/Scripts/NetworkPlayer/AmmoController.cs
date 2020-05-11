0abb59edfc1d85259ad9c812f366eda037ea4f26
filipton12 <37213766+filipton12@users.noreply.github.com>
2020-05-11 11:03:20 +0200

    New Ammo System
    Ammo is not value, ammo is magazines with amount of ammo

--- Assets/Scripts/NetworkPlayer/AmmoController.cs
+++ Assets/Scripts/NetworkPlayer/AmmoController.cs
@@ -5,19 +5,46 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
+[Serializable]
+public struct AmmoMagazine
+{
+    public int InMagazine;
+
+    public AmmoMagazine(int AmmoAmount)
+    {
+        InMagazine = AmmoAmount;
+    }
+}
+
+public class SyncAmmoMagazines : SyncList<AmmoMagazine> { }
+
 public class AmmoController : NetworkBehaviour
 {
     [SyncVar]
     public int MaxInMagazine = 30;
     [SyncVar]
-    public int MaxInPlayer = 100;
+    public int MaxMagazinesInPlayer = 5;
 
-    [SyncVar]
-    public int InMagazine = 30;
     [SyncVar]
     public int InPlayer = 100;
 
+    [SyncVar]
+    public AmmoMagazine CurrentMagazine;
+
+    public SyncAmmoMagazines AmmoMagazines = new SyncAmmoMagazines();
+
     public GameObject AmmoBoxPrefab;
+
+    [ServerCallback]
+    private void Start()
+    {
+        CurrentMagazine = new AmmoMagazine(MaxInMagazine);
+
+        for(int i = 0; i < MaxMagazinesInPlayer; i++)
+        {
+            AmmoMagazines.Add(new AmmoMagazine(MaxInMagazine));
+        }
+    }
 
     private void Update()
     {
@@ -27,8 +54,7 @@ public class AmmoController : NetworkBehaviour
             {
                 CmdReload();
             }
-
-            LocalSceneObjects.singleton.AmmoText.text = $"<color={(InMagazine < 6 ? "red" : "#51FF00")}>{InMagazine}</color> <color=#FF3F00>/</color> <color={(InPlayer < MaxInMagazine ? "red" : "#51FF00")}>{InPlayer}</color>";
+            LocalSceneObjects.singleton.AmmoText.text = $"<color={(CurrentMagazine.InMagazine < 6 ? "red" : "#51FF00")}>{CurrentMagazine.InMagazine}</color> <color=#FF3F00>/</color> <color={(InPlayer < MaxInMagazine ? "red" : "#51FF00")}>{InPlayer}</color>";
         }
     }
 
@@ -37,27 +63,40 @@ public class AmmoController : NetworkBehaviour
     {
         if(InPlayer > 0)
         {
-            if (InMagazine > 0)
+            if (CurrentMagazine.InMagazine > 0)
             {
                 GameObject gb = Instantiate(AmmoBoxPrefab, transform.position, Quaternion.Euler(0, 0, 0));
                 NetworkServer.Spawn(gb);
-                gb.GetComponent<AmmoBox>().InMagazine = InMagazine;
+                gb.GetComponent<AmmoBox>().InMagazine = CurrentMagazine.InMagazine;
             }
 
-            int min = InPlayer < MaxInMagazine ? InPlayer : MaxInMagazine;
-            InPlayer -= min;
-            InMagazine = min;
+            int min = AmmoMagazines[0].InMagazine;
+            AmmoMagazines.RemoveAt(0);
+            CurrentMagazine.InMagazine = min;
+            RefreshAllInPlayerAmmo();
         }
+    }
+
+    [ServerCallback]
+    public void RefreshAllInPlayerAmmo()
+    {
+        int inPlayer = 0;
+        foreach (AmmoMagazine am in AmmoMagazines)
+        {
+            inPlayer += am.InMagazine;
+        }
+        InPlayer = inPlayer;
     }
 
     [Command]
     public void CmdPickupAmmoBox(GameObject gbAmmoBox)
     {
-        if((transform.position - gbAmmoBox.transform.position).magnitude < 3f)
+        if((transform.position - gbAmmoBox.transform.position).magnitude < 3f && AmmoMagazines.Count < MaxMagazinesInPlayer)
         {
-            InPlayer += gbAmmoBox.GetComponent<AmmoBox>().InMagazine;
+            AmmoMagazines.Add(new AmmoMagazine(gbAmmoBox.GetComponent<AmmoBox>().InMagazine));
             NetworkServer.UnSpawn(gbAmmoBox);
             NetworkServer.Destroy(gbAmmoBox);
+            RefreshAllInPlayerAmmo();
         }
     }
 }
