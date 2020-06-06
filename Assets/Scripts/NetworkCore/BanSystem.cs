a45aaafcb91d4ece6a5fd9ca0bd0fb66bb492178
filipton12 <37213766+filipton12@users.noreply.github.com>
2020-06-06 11:23:37 +0200

    0.0.2.2 - 0.0.2.3 (Part II)
    -Added map "so_revolution"
    -Added php login & register system (ready to implement to the game)

---
+++ Assets/Scripts/NetworkCore/BanSystem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BanSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(GetDUID());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public string GetDUID()
+    {
+        return SystemInfo.deviceUniqueIdentifier;
+    }
+}
